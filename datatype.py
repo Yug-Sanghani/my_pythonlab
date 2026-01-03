b6ba42f55d044c723076076f6d1ea9d41b24d065
Yug-Sanghani <yugsanghani123@gmail.com>
2026-01-03 11:20:54 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,18 @@
+#print("hellowwwww")
+"""x=50
+y=44
+print(x+y)"""
+"""x=44.4
+print(x)"""
+"""x=yug4467
+print(x)"""
+"""x=["pani puri","ragado","pav bhaghi","mavo"]
+print(x)"""
+"""x=True
+print(x)"""
+"""x=("banana shake","orange juice","butter","shiro")
+print(x)"""
+"""x=({"black lebal","red bull","jaislmer","royal stage","bakkate"})
+print(x)"""
+x=range(4)
+print(x)
