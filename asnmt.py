4b8bd5671ebe4a8d785309c5b3a69d29d1a4c155
Yug-Sanghani <yugsanghani123@gmail.com>
2026-01-21 10:24:45 +0530

    Add files via upload

---
+++ asnmt.py
@@ -0,0 +1,59 @@
+#a="yug patel"
+#print(a)
+
+#2.add and sum
+#a = int(input("Enter first value: "))
+#b = int(input("Enter second value: "))
+#print("Sum =", a + b)
+
+#odd or even
+#num = int(input("Enter a number: "))
+#print("Even" if num % 2 == 0 else "Odd")
+
+# check leap year
+#year = int(input("Enter a year: "))
+
+#if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+   #print("Leap year")
+#else:
+    #print("Not a leap year")
+
+#pi value
+#from math import pi
+#print(pi)
+
+#constant
+#my_constant=44
+#print(my_constant)
+
+#square
+#num = int(input("Enter a number: "))
+#print(num ** 2)
+
+
+#area of redius
+#import math
+#radius = float(input("Enter radius: "))
+#area = math.pi * radius ** 2
+#print("Area of the circle is:", area)
+
+#data type
+#x = 10
+#print(isinstance(x, int))  
+
+#math function
+#import math
+#print(math.sqrt(25))
+
+#power
+#a = int(input("Enter base: "))
+#b = int(input("Enter power: "))
+#print(a ** b)
+
+#positive or nagative
+#num = int(input("Enter a number: "))
+#sprint("Positive" if num > 0 else "Negative" if num < 0 else "Zero")
+
+
+
+
