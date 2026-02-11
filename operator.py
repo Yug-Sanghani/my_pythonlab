c92e61f5cb9258cf9214da90344bb40f56257ac2
Yug-Sanghani <yugsanghani123@gmail.com>
2026-02-11 10:41:55 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,57 @@
+# arethmetic operator
+# a=10
+#b=10
+#print(a+b)
+
+
+#assignment operator
+#d=44
+#print(d)
+
+
+
+#unaray operator
+#a=44
+#print(-a)
+
+
+
+#relational operator
+#c=4
+#d=-4
+#print(c<=d)
+#print(d>=c)
+
+
+#logical operator
+#a=True
+#b=False
+#print(a and b)
+
+
+#bitwise operator
+#a=9
+#b=4
+#print(a|b)
+
+#boolean
+#is_pass=True
+#print(is_pass)
+
+
+#bitwise operator
+#a=4
+#b=3
+#print(a&b)
+
+
+#identity operator
+#x=10
+#p=16
+#print(x is p)
+
+
+
+#membership operator
+#m=[1,2,3,4,5]
+#print(8 in m)
