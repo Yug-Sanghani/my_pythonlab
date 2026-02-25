692545f900a2e2974355c018400305ded87d2d7a
Yug-Sanghani <yugsanghani123@gmail.com>
2026-02-25 11:02:18 +0530

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,19 @@
+#if.......else
+"""age=int(input("enter your age"))
+if age>=18:
+    print("yes you are eligable")
+else:
+    print("sorry ypou arenot eligable")"""
+
+
+
+
+
+#if.......elif......else
+"""marks=float(input("enter your marks"))
+if marks>=90:
+    print("you will grade a")
+elif marks>=60:
+    print("ypu will grade b")
+else:
+        print("bettar luck for next time")"""
