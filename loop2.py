e3e4e78c756135e063ed17dc1b8bbed646ffb407
Yug-Sanghani <yugsanghani123@gmail.com>
2026-03-02 11:08:30 +0530

    Add files via upload

---
+++ loop2.py
@@ -0,0 +1,32 @@
+#liesence
+"""age=int(input("enter your age:"))
+if age>=18:
+    print("yes you wiil apply for lisence")
+else:
+    print("sorry you are not alliped for lisence")"""
+    
+    
+    
+    
+    
+#positive   or   negarive  number
+"""number=float(input("enter your choise:"))
+if number>=1:
+    print("positive")
+    
+
+else:
+    print("negative")""""
+    
+    
+    
+    
+#odd  or   even
+a=float(input("enter your choise:"))
+if a=%2=0:
+    print("even")
+    
+else:
+    print("odd")
+    
+
