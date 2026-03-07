97a024fcad72915532abd9ead612177e45510f91
Yug-Sanghani <yugsanghani123@gmail.com>
2026-03-07 10:56:44 +0530

    Add files via upload

---
+++ array.py
@@ -0,0 +1,33 @@
+from array import array
+        #lengh
+#a=array('i',[10,20,30])
+#print(len(a))
+
+
+        #insert
+#q=array('i',[10,20,30])
+#q.insert(2, 12)
+#print(q)
+
+
+        #remove
+#c=array('i',[10,20,30,40,50,60,70,80,90,100])
+#c.remove(100)
+#print(c)
+
+
+        #pop
+#f=array('i',[10,20,30,40,50,60,70,80,90,100])
+#x=f.pop()
+#print("remove :",x)
+#print(f)
+
+
+        #Index
+#p=array('i',[10,20,30,40,50,])
+#print(p.index(40))
+
+
+        #count
+#ar=array('i',[10,20,30,40,50,20,20.20,20,20])
+#print(ar.count(20))
