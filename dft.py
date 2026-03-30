1046d8a171b32d5eca6f4de657e465b4abb45ce1
Yug-Sanghani <yugsanghani123@gmail.com>
2026-03-30 11:09:41 +0530

    Add files via upload

---
+++ dft.py
@@ -0,0 +1,21 @@
+"""# 1. 
+
+def greet(name="guest"):
+    print("hello",name)
+greet("alice")
+greet()"""
+
+#2.
+"""def add(a,b=4):
+    print("sum :",a+b)
+add(10,20)
+add(45)"""
+
+
+
+#3.squre
+def sqr(num,wxp=2):
+    return nem**wxp
+print(sqr(5,3))
+print(sqr(5,7))
+print(sqr(1,2))
