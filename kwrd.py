3e2fbbbd80141df9c86f94d329d8f4ce902bd0c6
Yug-Sanghani <yugsanghani123@gmail.com>
2026-03-31 08:02:55 +0530

    Create kwrd.py

---
+++ kwrd.py
@@ -0,0 +1,13 @@
+"""def stud_info(name,age,city):
+    print("nme :",name)
+    print("age :",age)
+    print("city :",city)
+stud_info(age=19,city="tankara",name="yug raj")"""
+
+#2.mixing positon
+"""def disp(a,b,c):
+    print("a=",a)
+    print("b=",b)
+    print("c=",c)
+    
+disp(1,c=44,b=40)"""
